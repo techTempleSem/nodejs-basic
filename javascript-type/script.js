fb148c7634380e8c68fa12cbcf9f7b960fd0a4fc
techTempleSem <techTempleSem@gmail.com>
2024-03-06 22:40:28 +0900

    first commit

---
+++ javascript-type/script.js
@@ -0,0 +1,22 @@
+const name = "lee";
+
+const age = 30;
+
+const hasJob = true;
+
+const car = null;
+
+let anything;
+
+const sym = Symbol();
+
+const hobbies = ["books", "walking"];
+
+const address = {
+    province: "서울",
+    city: "서초구",
+};
+
+console.log(typeof hobbies);
+console.log(Array.isArray(hobbies));
+console.log(Array.isArray(address));
---
+++ var, let, const/script.js
@@ -0,0 +1,70 @@
+/*
+var a = 1;
+console.log(a);
+
+var a = 10;
+console.log(a);
+
+a = 100;
+console.log(a);
+*/
+
+/*
+let a = 1;
+console.log(a);
+
+//error
+// let a = 10;
+// console.log(a);
+
+a = 100;
+console.log(a);
+*/
+
+/*
+const a = 1;
+console.log(a);
+
+//error
+// const a = 10;
+// console.log(a);
+
+//error
+// a = 100;
+// console.log(a);
+*/
+
+/*
+function func_var() {
+    if (true) {
+        var a = "a";
+        console.log(a);
+    }
+    console.log(a);
+}
+console.log(a);
+func_var();
+*/
+
+/*
+function func_let() {
+    if (true) {
+        let a = "a";
+        console.log(a);
+    }
+    console.log(a);
+}
+func_let();
+*/
+
+/*
+console.log(greeting);
+// var greeting = "hello";
+let greeting = "hello";
+
+*/
+
+func();
+function func() {
+    console.log("hoisting test");
+}
