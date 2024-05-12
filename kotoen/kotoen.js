b123a659fd1508400c91b0b6c482f60bfe304560
techTempleSem <techTempleSem@gmail.com>
2024-05-12 09:48:52 +0900

    ko <-> en

---
+++ kotoen/kotoen.js
@@ -0,0 +1,40 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin, // 터미널에서 입력
+    output: process.stdout, // 터미널에 출력
+});
+
+let ko = ["ㄱ","ㄲ","ㄳ","ㄴ","ㄵ","ㄶ","ㄷ","ㄸ","ㄹ","ㄺ","ㄻ","ㄼ","ㄽ","ㄾ","ㄿ","ㅀ","ㅁ","ㅂ","ㅃ","ㅄ","ㅅ","ㅆ","ㅇ","ㅈ","ㅉ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ","ㅏ","ㅐ","ㅑ","ㅒ","ㅓ","ㅔ","ㅕ","ㅖ","ㅗ","ㅘ","ㅙ","ㅚ","ㅛ","ㅜ","ㅝ","ㅞ","ㅟ","ㅠ","ㅡ","ㅢ","ㅣ"]
+let en = ["r","R","rt","s","sw","sg","e","E","f","fr","fa","fq","ft","fx","fv","fg","a","q","Q","qt","t","T","d","w","W","c","z","x","v","g","k","o","i","O","j","p","u","P","h","hk","ho","hl","y","n","nj","np","nl","b","m","ml","l"]
+function singleKoToEn(ko){
+    let uni = ko.charCodeAt(0) - 'ㄱ'.charCodeAt(0);
+    process.stdout.write(en[uni]);
+}
+function multiKoToEn(ko){
+    let first=["ㄱ","ㄲ","ㄴ","ㄷ","ㄸ","ㄹ","ㅁ","ㅂ","ㅃ","ㅅ","ㅆ","ㅇ","ㅈ","ㅉ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"]
+    let second=["ㅏ","ㅐ","ㅑ","ㅒ","ㅓ","ㅔ","ㅕ","ㅖ","ㅗ","ㅘ","ㅙ","ㅚ","ㅛ","ㅜ","ㅝ","ㅞ","ㅟ","ㅠ","ㅡ","ㅢ","ㅣ"]
+    let third=["","ㄱ","ㄲ","ㄳ","ㄴ","ㄵ","ㄶ","ㄷ","ㄹ","ㄺ","ㄻ","ㄼ","ㄽ","ㄾ","ㄿ","ㅀ","ㅁ","ㅂ","ㅄ","ㅅ","ㅆ","ㅇ","ㅈ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"]
+    let uni = ko.charCodeAt(0) - '가'.charCodeAt(0);
+    singleKoToEn(first[Math.floor(uni/588)]);
+    singleKoToEn(second[Math.floor(uni%588/28)]);
+    if(uni%28==0) return;
+    singleKoToEn(third[Math.floor(uni%28)]);
+}
+
+
+rl.on('line', (answer) => {
+
+    for(char of answer){
+        if(char >= 'ㄱ' && char <= 'ㅣ'){
+            singleKoToEn(char)
+        }
+        else if(char >= '가' && char <= '힣'){
+            multiKoToEn(char)
+        } else {
+            process.stdout.write(char);
+        }
+    }
+
+    rl.close();
+});
